ce8b0b06de004eedc88d859038a15ed7b50cc095
Sohitri216 <mithi216@gmail.com>
2019-05-05 22:36:32 +0530

    dummy api call added with axions using weather api

--- my-cart/src/components/pages/Details.js
+++ my-cart/src/components/pages/Details.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import PostData from '../../JSON/data.json';
+import axios from'axios';
 
 class Details extends Component {
     componentDidMount() {
-        // const { match: { params } } = this.props;
-        console.log('hii',this.props);
+        console.log('hii',this.props, PostData);
+        axios.get('http://api.openweathermap.org/data/2.5/forecast'+ "?lat=" + '22.04' + "&lon=" + '88.12' + "&cnt=" + 5 + "&APPID=" + '59f3090a217f217c1fdb4d9e0edfee66')
+        .then(res=>console.log(res));
     }
     render() {
-        console.log(this.props.match)
+        // console.log(this.props.match)
         return (
            
             <React.Fragment>
